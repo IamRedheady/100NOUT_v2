87c66e878173dbe395c0732860a0ecf914fa6f5e
Redheady <iam@redheady.ru>
2023-03-04 22:30:25 +0300

    fix product and home, catalog pages

--- src/js/app.js
+++ src/js/app.js
@@ -4,5 +4,24 @@ import '../scss/app.scss';
 
 // Elements
 import "./elements/dropdown";
-import "./elements/catalogModes";
+import "./elements/intro";
+const changePhotos = (e, productClass) => {
+    if (window.innerWidth > 1000) {
+        const slides = document.querySelectorAll(`.${productClass} .swiper-slide`)
+
+        const currentArray = slides
+        const currentWidth = e.target.offsetWidth
+        const currentCursorPosition = e.layerX
+        const currentLayers = currentArray.length
+        const currentLayerWidth = currentWidth / currentArray.length
+        const dots = document.querySelectorAll(`.${productClass} .swiper-pagination-bullet`)
+
+        for (let i = 0; i < currentLayers; i++) {
+            if (currentCursorPosition > currentLayerWidth * i && currentCursorPosition < currentLayerWidth * (i + 1)) {
+                dots[i].click()
+                console.log(i)
+            }
+        }
+    }
+}
 
---
+++ src/js/elements/intro.js
@@ -0,0 +1,37 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// Intro Slider
+const introSlider = new Swiper('.intro-slider .swiper-container', {
+    slidesPerView: 1,
+    effect: 'fade',
+    autoplay: {
+        delay: 5000,
+    },
+    fadeEffect: {
+        crossFade: true
+    },
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+        nextEl: '.intro-slider .swiper-button-next',
+        prevEl: '.intro-slider .swiper-button-prev',
+    },
+    pagination: {
+        el: '.intro-slider .swiper-pagination',
+        dynamicBullets: true,
+        clickable: true
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        620: {
+            pagination: {
+                dynamicBullets: false
+            }
+        },
+    }
+});
+
