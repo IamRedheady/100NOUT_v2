ed7b5f094302911b8b0b90d9d8197505c197c58e
Redheady <96238052+IamRedheady@users.noreply.github.com>
2024-04-05 16:34:58 +0300

    Фикс формы заявки

--- src/js/app.js
+++ src/js/app.js
@@ -14,8 +14,7 @@ let tg = {
     token: "5360345876:AAEyM_sckiSJyjD3ci4QLtPfstR4Uxn9tDk",
     chat_id: "-1001933568755"
 }
-function sendMessage(event)
-{
+function sendMessage(event) {
     const url = `https://api.telegram.org/bot${tg.token}/sendMessage`
 
     const tgForm = document.querySelector('.tg-form')
@@ -23,15 +22,18 @@ function sendMessage(event)
         tel = tgForm.querySelector('[name="ftel"]'),
         txt = tgForm.querySelector('[name="ftext"]');
 
-    const data = {
-        name: name.value,
-        tel: tel.value,
-        txt: txt.value,
-    };
+    const msg = `Заявка со страницы скупки 100nout.by! 
+
+Имя: ${name.value}
+Телефон: ${tel.value}
+Сообщение: ${txt.value}
+
+Отправлено со страницы ${window.location.href}
+`
 
     const obj = {
         chat_id: tg.chat_id,
-        text: data
+        text: msg
     };
 
     const xht = new XMLHttpRequest();
@@ -39,12 +41,11 @@ function sendMessage(event)
     xht.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     xht.send(JSON.stringify(obj));
 }
-const formSubmit = document.querySelector(".tg-form-submit");
+const formSubmit = document.querySelector(".js-tg-form");
 
-
-formSubmit.addEventListener('submit', function(event) {
+formSubmit.addEventListener('submit', function (e) {
+    e.preventDefault();
     sendMessage();
-    event.preventDefault();
 });
 
 // Elements
