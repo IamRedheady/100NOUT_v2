8d601631500870a685280e805d4bb7fbf21d01fb
Алескандр Петров <alex_petrov_cr@yahoo.com>
2024-04-05 16:15:13 +0300

    Форма

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,43 @@ const maskOptions = {
 const mask = IMask(obmenTel, maskOptions);
 
 
+let tg = {
+    token: "5360345876:AAEyM_sckiSJyjD3ci4QLtPfstR4Uxn9tDk",
+    chat_id: "-1001933568755"
+}
+function sendMessage(event)
+{
+    const url = `https://api.telegram.org/bot${tg.token}/sendMessage`
+
+    const tgForm = document.querySelector('.tg-form')
+    const name = tgForm.querySelector('[name="fname"]'),
+        tel = tgForm.querySelector('[name="ftel"]'),
+        txt = tgForm.querySelector('[name="ftext"]');
+
+    const data = {
+        name: name.value,
+        tel: tel.value,
+        txt: txt.value,
+    };
+
+    const obj = {
+        chat_id: tg.chat_id,
+        text: data
+    };
+
+    const xht = new XMLHttpRequest();
+    xht.open("POST", url, true);
+    xht.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    xht.send(JSON.stringify(obj));
+}
+const formSubmit = document.querySelector(".tg-form-submit");
+
+
+formSubmit.addEventListener('submit', function(event) {
+    sendMessage();
+    event.preventDefault();
+});
+
 // Elements
 import "./elements/dropdown";
 import "./elements/intro";
